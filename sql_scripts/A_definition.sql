07db823c1cb28e22e8c06a6c53ed65fa1b09e37d
P G <pgalewicz@gmail.com>
2020-03-08 16:33:37 +0100

    renamed constraints in database definition script

--- sql_scripts/A_definition.sql
+++ sql_scripts/A_definition.sql
@@ -14,7 +14,7 @@ CREATE TABLE hotel.dbo.city (
 	city_id		INT IDENTITY(1,1),
 	name		VARCHAR(25) NOT NULL,
 
-	CONSTRAINT miasta_kg PRIMARY KEY (city_id)
+	CONSTRAINT city_pk PRIMARY KEY (city_id)
 );
 GO
 
@@ -25,7 +25,7 @@ CREATE TABLE hotel.dbo.room (
 	has_bathtub		BIT,
 	has_safe		BIT,
 
-	CONSTRAINT pokoje_kg PRIMARY KEY (room_id)
+	CONSTRAINT room_pk PRIMARY KEY (room_id)
 );
 GO
 
@@ -39,10 +39,10 @@ CREATE TABLE hotel.dbo.client (
 	phone			VARCHAR(15) NOT NULL,
 	type			INT NOT NULL,
 
-	CONSTRAINT klienci_kg PRIMARY KEY (client_id),
-	CONSTRAINT klienci_ko_miasto FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
+	CONSTRAINT client_pk PRIMARY KEY (client_id),
+	CONSTRAINT client_city_fk FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
 
-	CONSTRAINT klienci_sprawdz_typ CHECK (type <= 3 AND type >= 1)
+	CONSTRAINT client_type_check CHECK (type <= 3 AND type >= 1)
 );
 GO
 
@@ -51,7 +51,7 @@ CREATE TABLE hotel.dbo.occupation (
 	name			VARCHAR(50)	NOT NULL,
 	minimum_wage	MONEY NOT NULL,
 
-	CONSTRAINT stanowisko_kg PRIMARY KEY (occupation_id)
+	CONSTRAINT occupation_pk PRIMARY KEY (occupation_id)
 )
 GO
 
@@ -68,12 +68,12 @@ CREATE TABLE hotel.dbo.employee (
 	occupation_id		INT NOT NULL,
 	wage				MONEY NOT NULL,
 
-	CONSTRAINT pracownicy_kg PRIMARY KEY (employee_id),
-	CONSTRAINT pracownicy_ko_miasto FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
-	CONSTRAINT pracownicy_ko_stanowisko FOREIGN KEY (occupation_id) REFERENCES hotel.dbo.occupation (occupation_id),
+	CONSTRAINT employee_pk PRIMARY KEY (employee_id),
+	CONSTRAINT employee_city_fk FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
+	CONSTRAINT employee_occupation_fk FOREIGN KEY (occupation_id) REFERENCES hotel.dbo.occupation (occupation_id),
 
-	CONSTRAINT pracownicy_sprawdz_daty CHECK (birth_date < employment_date),
---	CONSTRAINT pracownicy_sprawdz_placa CHECK (wage >= (SELECT s.minimum_wage FROM occupation AS s WHERE s.occupation_id = occupation_id))
+	CONSTRAINT employee_date_check CHECK (birth_date < employment_date),
+--	CONSTRAINT employee_wage_check CHECK (wage >= (SELECT s.minimum_wage FROM occupation AS s WHERE s.occupation_id = occupation_id))
 );
 GO
 
@@ -90,9 +90,9 @@ CREATE TABLE hotel.dbo.exemployee (
 	occupation_id		INT NOT NULL,
 	wage				MONEY NOT NULL,
 
-	CONSTRAINT byli_pracownicy_kg PRIMARY KEY (employee_id),
-	CONSTRAINT byli_pracownicy_ko_miasto FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
-	CONSTRAINT byli_pracownicy_ko_stanowisko FOREIGN KEY (occupation_id) REFERENCES hotel.dbo.occupation (occupation_id)
+	CONSTRAINT exemployee_pk PRIMARY KEY (employee_id),
+	CONSTRAINT exemployee_city_fk FOREIGN KEY (city) REFERENCES hotel.dbo.city (city_id),
+	CONSTRAINT exemployee_occupation_fk FOREIGN KEY (occupation_id) REFERENCES hotel.dbo.occupation (occupation_id)
 );
 GO
 
@@ -104,11 +104,11 @@ CREATE TABLE hotel.dbo.booking (
 	start_date		DATE NOT NULL,
 	day_count		INT NOT NULL,
 
-	CONSTRAINT rezerwacje_kg PRIMARY KEY(booking_id),
-	CONSTRAINT rezerwacje_ko_pokoj FOREIGN KEY (room_id) REFERENCES hotel.dbo.room (room_id),
-	CONSTRAINT rezerwacje_ko_klient FOREIGN KEY (client_id) REFERENCES hotel.dbo.client (client_id),
+	CONSTRAINT booking_pk PRIMARY KEY(booking_id),
+	CONSTRAINT booking_room_fk FOREIGN KEY (room_id) REFERENCES hotel.dbo.room (room_id),
+	CONSTRAINT booking_client_fk FOREIGN KEY (client_id) REFERENCES hotel.dbo.client (client_id),
 
---	CONSTRAINT rezerwacje_sprawdz_il_os CHECK (man_count <= (SELECT p.capacity FROM room AS p WHERE p.room_id = room_id))
+--	CONSTRAINT booking_room_capacity_check CHECK (man_count <= (SELECT p.capacity FROM room AS p WHERE p.room_id = room_id))
 );
 GO
 
@@ -120,8 +120,8 @@ CREATE TABLE hotel.dbo.past_booking (
 	start_date		DATE NOT NULL,
 	end_date		DATE NOT NULL,
 
-	CONSTRAINT byle_rezerwacje_kg PRIMARY KEY(booking_id),
-	CONSTRAINT byle_rezerwacje_ko_pokoj FOREIGN KEY (room_id) REFERENCES hotel.dbo.room (room_id),
-	CONSTRAINT byle_rezerwacje_ko_klient FOREIGN KEY (client_id) REFERENCES hotel.dbo.client (client_id),
+	CONSTRAINT past_booking_pk PRIMARY KEY(booking_id),
+	CONSTRAINT past_booking_room_fk FOREIGN KEY (room_id) REFERENCES hotel.dbo.room (room_id),
+	CONSTRAINT past_booking_client_fk FOREIGN KEY (client_id) REFERENCES hotel.dbo.client (client_id),
 );
 GO
