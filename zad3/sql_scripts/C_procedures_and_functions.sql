64807b315b7f5450eb7144985489a93b08a147e8
shreq <shrek.kresh@o2.pl>
2020-05-10 19:16:24 +0200

    fixed export

--- zad3/sql_scripts/C_procedures_and_functions.sql
+++ zad3/sql_scripts/C_procedures_and_functions.sql
@@ -22,79 +22,80 @@ if exists (select 1 from sysobjects where name='export_xml')
     drop procedure export_xml
 go
 
-create procedure export_xml (@table nvarchar(50), @path nvarchar(200)) as
+create procedure export_xml (@table nvarchar(50), @path nvarchar(200), @instance_name nvarchar(100)) as
 begin
     declare @cmd nvarchar(400);
     set nocount on
-    set @cmd = N'bcp "select * from ' + @table + N' for xml auto, elements" queryout "' + @path + N'" -c -T'
+    set @cmd = N'bcp "select * from ' + @table + N' for xml auto, elements" queryout "' + @path + N'" -w -t; -T -S ' + @instance_name
     exec xp_cmdshell @cmd
     set nocount off
 end
 go
 
+-- procedure #3: view_dishes
 if exists (select 1 from sysobjects where name='view_dishes')
     drop procedure view_dishes
 go
 
 create procedure view_dishes (@file_id int) as
 begin
-	declare @xml as xml
-	declare @doc as int
+    declare @xml as xml
+    declare @doc as int
 
-	select @xml = document from files where id = @file_id
+    select @xml = document from files where id = @file_id
 
-	exec sp_xml_preparedocument @doc output, @xml
+    exec sp_xml_preparedocument @doc output, @xml
 
-	select * from openxml(@doc, 'cookbook/dishes/dish', 2) 
-	with (dish_id varchar(10) '@did',
-		  recipie_id varchar(10) '@rid',
-		  category_id varchar(10) '@cid',
-		  name text 'name')
+    select * from openxml(@doc, 'cookbook/dishes/dish', 2)
+    with (dish_id varchar(10) '@did',
+          recipie_id varchar(10) '@rid',
+          category_id varchar(10) '@cid',
+          name text 'name')
 
-	exec sp_xml_removedocument @doc
+    exec sp_xml_removedocument @doc
 end
 go
 
-
+-- procedure #4: view_recipes
 if exists (select 1 from sysobjects where name='view_recipes')
     drop procedure view_recipes
 go
 
 create procedure view_recipes (@file_id int) as
 begin
-	declare @xml as xml
-	declare @doc as int
+    declare @xml as xml
+    declare @doc as int
 
-	select @xml = document from files where id = @file_id
+    select @xml = document from files where id = @file_id
 
-	exec sp_xml_preparedocument @doc output, @xml
+    exec sp_xml_preparedocument @doc output, @xml
 
-	select * from openxml(@doc, 'cookbook/recipes/recipe', 2) 
-	with (recipe_id varchar(10) '@rid',
-		  content text '.')
+    select * from openxml(@doc, 'cookbook/recipes/recipe', 2)
+    with (recipe_id varchar(10) '@rid',
+          content text '.')
 
-	exec sp_xml_removedocument @doc
+    exec sp_xml_removedocument @doc
 end
 go
 
-
+-- procedure #5: view_categories
 if exists (select 1 from sysobjects where name='view_categories')
     drop procedure view_categories
 go
 
 create procedure view_categories (@file_id int) as
 begin
-	declare @xml as xml
-	declare @doc as int
+    declare @xml as xml
+    declare @doc as int
 
-	select @xml = document from files where id = @file_id
+    select @xml = document from files where id = @file_id
 
-	exec sp_xml_preparedocument @doc output, @xml
+    exec sp_xml_preparedocument @doc output, @xml
 
-	select * from openxml(@doc, 'cookbook/categories/category', 2) 
-	with (category_id varchar(10) '@cid',
-		  name text '.')
+    select * from openxml(@doc, 'cookbook/categories/category', 2)
+    with (category_id varchar(10) '@cid',
+          name text '.')
 
-	exec sp_xml_removedocument @doc
+    exec sp_xml_removedocument @doc
 end
 go
--- zad3/sql_scripts_manual/usage_examples.sql
+++ zad3/sql_scripts_manual/usage_examples.sql
@@ -1,22 +1,18 @@
 -- procedure #1: load_from_file
 select * from cookbook..files
 go
-exec cookbook..import_xml 'C:\Users\Tomek\Repos\studia\RMSBD\zad3\xmls\full.xml';
+exec cookbook..import_xml 'P:\xmls\full.xml';
 select * from cookbook..files
 go
 
 -- procedure #2:
-exec cookbook..export_xml 'cookbook..files', "C:\Users\Tomek\Repos\studia\RMSBD\zad3\xmls\sav.xml";
+exec cookbook..export_xml 'cookbook..files', 'P:\xmls\sav.xml', 'DESKTOP-TAVGN6D\SQLEXPRESS';
 
 -- procedure #3: view_dishes <file_id>
-exec cookbook..view_dishes 3
+exec cookbook..view_dishes 1;
 
 -- procedure #4: view_recipes <file_id>
-exec cookbook..view_recipes 2
+exec cookbook..view_recipes 1;
 
 -- procedure #5: view_categories <file_id>
-exec cookbook..view_categories 2
-
-
--- EXEC xp_cmdshell 'whoami /all'
--- EXEC xp_cmdshell 'bcp "SELECT * FROM cookbook..files FOR XML AUTO, ELEMENTS" queryout "P:\xmls\sav.xml" -c -w -t -T -S DESKTOP-TAVGN6D\SQLEXPRESS'
+exec cookbook..view_categories 1;
