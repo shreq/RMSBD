885639800a1ed73a4374ae087259b62ed6773de8
pawci0 <pgalewicz@gmail.com>
2020-06-01 17:34:40 +0200

    zad4 - added locations table

--- zad4/sql_scripts/A_definition.sql
+++ zad4/sql_scripts/A_definition.sql
@@ -13,14 +13,26 @@ create table geo..parks (
     id          int identity(1, 1),
     park_name   varchar(100),
     coords      geography,
-    constraint const primary key clustered (
+
+    constraint c_pk_park primary key clustered (
         id  asc
-    ) --with (
-    --    pad_index = off,
-    --    statistics_norecompute = off,
-    --    ignore_dup_key = off
-    --)
+    )
 );
 go
 
 create spatial index idx_sth on geo..parks(coords);
+go
+
+create table geo..locations (
+	id			int identity(1, 1),
+	park_id		int not null,
+	coords		geography,
+	description	varchar(100),
+	type		varchar(50),
+
+	constraint c_pk_locations primary key clustered (
+        id  asc
+    ),
+	constraint c_fk_park_id foreign key (park_id) references geo..parks (id)
+);
+go
