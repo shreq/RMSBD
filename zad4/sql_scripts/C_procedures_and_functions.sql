7608644c2cf7076b582b2870c538f305792ba75e
pawci0 <pgalewicz@gmail.com>
2020-06-01 18:41:25 +0200

    zad4, procedures for part 3

--- zad4/sql_scripts/C_procedures_and_functions.sql
+++ zad4/sql_scripts/C_procedures_and_functions.sql
@@ -1,19 +1,58 @@
 use geo
 go
 
--- procedure #1: import_xml
+-- procedure #1: print_readable
 if exists (select 1 from sysobjects where name='print_readable')
     drop procedure print_readable
 go
 
-create procedure print_readable (@id int = null) as
+create procedure print_readable (@park_id int = null) as
 begin
     set nocount on
-    if @id is null
+    if @park_id is null
         select id, park_name, coords.Lat as 'Latitude', coords.Long as 'Longitude' from geo..parks;
     else
         select id, park_name, coords.Lat as 'Latitude', coords.Long as 'Longitude' from geo..parks
-        where @id = geo..parks.id;
+        where @park_id = geo..parks.id;
+    set nocount off
+end
+go
+
+-- procedure #2: add_point_of_interest
+if exists (select 1 from sysobjects where name='add_point_of_interest')
+    drop procedure add_point_of_interest
+go
+
+create procedure add_point_of_interest (@park_id int, @x_cord float, @y_cord float, @description varchar(100)) as
+begin
+    set nocount on
+    insert into geo..locations values (@park_id, geography::Point(@x_cord, @y_cord, 4326), @description, 'POI') 
+    set nocount off
+end
+go
+
+-- procedure #3: add_route
+if exists (select 1 from sysobjects where name='add_route')
+    drop procedure add_route
+go
+
+create procedure add_route (@park_id int, @x_start float, @y_start float, @x_stop float, @y_stop float, @description varchar(100)) as
+begin
+    set nocount on
+    insert into geo..locations values (@park_id, geography::STLineFromText('LINESTRING(' + str(@y_start) + ' ' + str(@x_start) + ', ' + str(@y_stop) + ' ' + str(@x_stop) + ' )', 4326), @description, 'ROUTE')
+    set nocount off
+end
+go
+
+-- procedure #4: add_restricted_area
+if exists (select 1 from sysobjects where name='add_restricted_area')
+    drop procedure add_restricted_area
+go
+
+create procedure add_restricted_area (@park_id int, @restricted_polygon geography, @description varchar(100)) as
+begin
+    set nocount on
+    insert into geo..locations values (@park_id, @restricted_area, @description, 'RESTRICTED')
     set nocount off
 end
 go
--- zad4/sql_scripts_manual/usage_examples.sql
+++ zad4/sql_scripts_manual/usage_examples.sql
@@ -1,2 +1,13 @@
 -- procedure #1: print_readable
-exec geo..print_readable;
+exec geo..print_readable 2;
+
+-- procedure #2 add_point_of_interest
+exec geo..add_point_of_interest 1, 44.60, -110.50, 'toilet'
+
+-- procedure #3 add_route
+exec geo..add_point_of_interest 1, 44.60, -110.50, 44.65, -110.45, 'shortest path'
+
+-- procedure #4 add_restricted_area
+exec geo..add_point_of_interest 1, 44.60, -110.50, 'toilet'
+
+select * from geo..locations
